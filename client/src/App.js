1b8645796d65d232c37d79fcbb5989ad12c2c6bf
Himagiri Nandan <himagirinandantuduru@gmail.com>
2025-02-07 11:48:17 +0530

    architecture setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-   <></>
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<h1>Welcome to our Website</h1>} />
+    </Routes>
+   </BrowserRouter>
   );
 }
 
