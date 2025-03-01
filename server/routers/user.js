9acf16a1085ed7ac4cbed6d9895ab1d52713e61c
Himagiri Nandan <himagirinandantuduru@gmail.com>
2025-03-01 18:02:08 +0530

    changed login route

--- server/routers/user.js
+++ server/routers/user.js
@@ -319,8 +319,7 @@ router.post("/login", async (req, res) => {
     console.log("User logged in:", user);
     const token = jwt.sign({ id: user._id }, "djbvunvuwheoufheowhfwuhefuhifwuehi", { expiresIn: "1d" });
     console.log("Token:", token);
-    res.cookie({
-      token,
+    res.cookie("token", token, {
       httpOnly: true,
       secure: true,
       sameSite: "none",
